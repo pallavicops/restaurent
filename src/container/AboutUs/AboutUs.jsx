1a359595932a8222ad3fd09613c2d12680e9edc2
Pallavi Kumari <75251279+pallavicops@users.noreply.github.com>
2023-12-15 13:50:18 +0530

    Initial commint with Navbar, Header, and AboutUs

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './AboutUs.css';
+
+import {images} from '../../constants';
+
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut beatae, ea necessitatibus magnam, quidem doloribus quos quo consequuntur ipsam, ex eligendi. Eveniet nam, similique iusto dicta id doloribus quis in.</p>
+      <button type='button' className='custom__button'>Know More</button>
+      
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+    <img src={images.knife} alt="about_Knife" />
+    </div>
+    <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut beatae, ea necessitatibus magnam, quidem doloribus quos quo consequuntur ipsam, ex eligendi. Eveniet nam, similique iusto dicta id doloribus quis in.</p>
+      <button type='button' className='custom__button'>Know More</button>
+      
+      </div>
+  
+
+    </div>
+   
+  
+  </div>
+);
+
+export default AboutUs;
